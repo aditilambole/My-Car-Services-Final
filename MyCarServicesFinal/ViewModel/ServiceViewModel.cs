d3a765dcbd53b37c4bb03d7224d2992b0810990f
Nidhzz <nidhz2015@gmail.com>
2019-07-29 12:58:04 +0530

    Add project files.

---
+++ MyCarServicesFinal/ViewModel/ServiceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MyCarServicesFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCarServicesFinal.ViewModel
+{
+    public class ServiceViewModel
+    {
+        public Car Car{ get; set; }
+        public IEnumerable<Service> Services { get; set; }
+        public Service Service { get; set; }
+        public IEnumerable<ServiceType> ServiceType { get; set; }
+
+    }
+}
